d8c5dfa81b4fcd72655896c9c073a4ef4b3de229
vibi-230 <vibin@turftown.in>
2021-02-08 03:16:08 +0530

    added home image

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
-
+import HomeImage from "../assets/homeImage.jpg";
+import FloatingImage1 from "../assets/homefloatingimage1.png";
 import "./Home.css";
 
 export const Home = () => {
   return (
     <div className="title-group">
       <h1>Custom title here</h1>
+      <div className="homeImage-container">
+        <img src={HomeImage} className="homeImage" />
+        <img src={FloatingImage1} className="floatingImage1" />
+      </div>
       <h2>Welcome to the website</h2>
     </div>
   );
