6bb19a759c58019c209ebf0270022c899747d3d6
vibi-230 <vibin@turftown.in>
2021-02-12 01:33:53 +0530

    added animation to contacts page

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { FaGithub, FaLinkedinIn, FaWhatsapp, FaTwitter } from "react-icons/fa";
 
 import "./Contacts.css";
 
 export const Contacts = () => {
-  return <div></div>;
+  return (
+    <div className="contacts-parent-container">
+      <div className="contacts-icons-container">
+        <span className="icon">
+          <FaGithub size="2.5rem" />
+        </span>
+        <span className="icon">
+          <FaLinkedinIn size="2.5rem" />
+        </span>
+        <span className="icon">
+          <FaWhatsapp size="2.5rem" />
+        </span>
+        <span className="icon">
+          <FaTwitter size="2.5rem" />
+        </span>
+      </div>
+      <div className="contacts-bottom-mirror"></div>
+    </div>
+  );
 };
